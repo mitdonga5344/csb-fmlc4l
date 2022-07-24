f1ff9ef12f9a14a014c1fcf31958bf97cbc2c40c
Code Sandbox <hello@codesandbox.io>
2022-07-24 10:13:00 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6e1fe25e2e78e632a0835acb228f8494.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07564d705d7ba2faf68722b5afbd8043",
+    "url": "https://mitdonga5344.github.io/csb-fmlc4l/index.html"
+  },
+  {
+    "revision": "2c266d0415b9455f72d7",
+    "url": "https://mitdonga5344.github.io/csb-fmlc4l/static/js/2.0b340d99.chunk.js"
+  },
+  {
+    "revision": "45eb927f7c8c5c864647",
+    "url": "https://mitdonga5344.github.io/csb-fmlc4l/static/js/main.b9aebe4a.chunk.js"
+  },
+  {
+    "revision": "36b594c4a7470dd74cf9",
+    "url": "https://mitdonga5344.github.io/csb-fmlc4l/static/js/runtime-main.a12dd46a.js"
+  }
+]);
